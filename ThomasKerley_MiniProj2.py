3282c5e8d81c9bfd17476ce5c2e9b946b6011b1c
tmkerley <70177694+tmkerley@users.noreply.github.com>
2022-09-25 12:40:00 -0600

    Switch to API Ninjas

--- ThomasKerley_MiniProj2.py
+++ ThomasKerley_MiniProj2.py
@@ -1,9 +1,15 @@
-# Import Faker.
-from faker import Faker
-# Import the WifiESSID class from Faker Wi-Fi ESSID.
-from faker_wifi_essid import WifiESSID
+# INF 601 Advanced Python Programming
+# Fall 2022
+# Thomas Kerley
+# Mini Project 2
+# Due 9/25/2022
 
-fake = Faker()                                                                                                      
-fake.add_provider(WifiESSID)                                                                                        
-for i in range(20):                                                                                                 
-    fake.wifi_essid()  
+import requests
+
+country = 'United States'
+api_url = 'https://api.api-ninjas.com/v1/inflation?country={}'.format(country)
+response = requests.get(api_url, headers={'X-Api-Key': 'XAnRBGt717T92RQT6/9rig==UD2M9TYjxtSswYLY'})
+if response.status_code == requests.codes.ok:
+    print(response.text)
+else:
+    print("Error:", response.status_code, response.text)
